9d5ef97443d7bf8b06f6ba7e4706196d213bcc51
Charles Breton <charles.bretn@gmail.com>
2019-02-04 20:15:51 +0800

    home page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home', to: 'pages#home', as: :home
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
