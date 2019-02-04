8576bfe1108d8657ab11b019890414ed468da8a2
Charles Breton <charles.bretn@gmail.com>
2019-02-04 17:32:44 +0800

    first routes about & contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
