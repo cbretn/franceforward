fe1f21f251a290253631df37c30aebf3b51b8f6c
Charles Breton <charles.bretn@gmail.com>
2019-02-14 16:39:57 +0800

    updated gems with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
+
   get 'home', to: 'pages#home', as: :home
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
