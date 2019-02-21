d6258778a37eb480fc59b8a336495563feb42e11
Charles Breton <charles.bretn@gmail.com>
2019-02-21 12:58:39 +0800

    messages and conversations views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :conversations, :foreign_key => :user1_id
 end
