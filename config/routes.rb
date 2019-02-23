359616cb479a6d87fcc0ab21a4f70449215334a3
Charles Breton <charles.bretn@gmail.com>
2019-02-23 15:44:37 +0800

    fixed themes#all route

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,6 @@ Rails.application.routes.draw do
     end
   end
 
-  # get '/themes', to: 'themes#all', as: themes
-  resource :themes, only: [] do
-    collection do
-      get 'all'
-    end
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'themes', to: 'themes#all', as: :themes
+
 end
