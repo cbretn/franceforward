a60c0cd561427a1205539327b7378f5386683459
Charles Breton <charles.bretn@gmail.com>
2019-02-28 22:31:27 +0800

    industrial seeds for categories themes descriptions discussions actions participations conversations and messages

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,6 +2,7 @@ class Action < ApplicationRecord
   # associations
   belongs_to :theme
   belongs_to :user
+  has_many :action_participations, dependent: :destroy
 
   # validations
   validates :title, presence: true
