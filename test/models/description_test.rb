615223ee14a90f689b99f069bc3efe035ea3c1de
Charles Breton <charles.bretn@gmail.com>
2019-03-08 17:25:07 +0100

    description tests

--- test/models/description_test.rb
+++ test/models/description_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class DescriptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "saves a description only if all prerequisites are met" do
+    description = Description.new
+    assert_not description.save, "saved a description without a theme"
+    description.theme = Theme.find_by(name: "Pollution")
+    assert_not description.save, "saved a description without a content"
+    description.content = "Content still under redaction"
+    assert description.save, "could not save a description"
+    assert description.destroy, "could not delete description"
+  end
+
+
 end
