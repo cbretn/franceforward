5936d62f225893ab6689558727e76c1366132c1b
Charles Breton <charles.bretn@gmail.com>
2019-05-13 13:54:24 +0200

    messages controller test

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
+
+  include Devise::Test::IntegrationHelpers
+
   setup do
     sign_in users(:hugo)
   end
@@ -10,12 +13,13 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get conversation_messages_path()
+    get conversation_messages_path(conversations(:vicurie))
     assert_response :success
   end
 
   test "should get create" do
-    get messages_create_url
+    post conversation_messages_path(conversations(:vicurie)),
+      params: {message: {body: "Hello, Marie, ça roule?"}}
     assert_response :success
   end
 
--- test/controllers/themes_controller_test.rb
+++ test/controllers/themes_controller_test.rb
@@ -1,14 +1,14 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class ThemesControllerTest < ActionDispatch::IntegrationTest
-#   test "should get index" do
-#     get themes_index_url
-#     assert_response :success
-#   end
+class ThemesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get themes_index_url
+    assert_response :success
+  end
 
-#   test "should get show" do
-#     get themes_show_url
-#     assert_response :success
-#   end
+  test "should get show" do
+    get themes_show_url
+    assert_response :success
+  end
 
-# end
+end
