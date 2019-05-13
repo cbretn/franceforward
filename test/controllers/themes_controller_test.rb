af2a9235e9f7a41206127db32f4c5658a9418562
Charles Breton <charles.bretn@gmail.com>
2019-05-13 14:07:54 +0200

    themes controller test

--- test/controllers/themes_controller_test.rb
+++ test/controllers/themes_controller_test.rb
@@ -2,13 +2,17 @@ require 'test_helper'
 
 class ThemesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get themes_index_url
+    get category_themes_path(categories(:environnement))
     assert_response :success
   end
 
   test "should get show" do
-    get themes_show_url
+    get category_theme_path(categories(:environnement), themes(:pollution))
     assert_response :success
   end
 
+  test "should get all" do
+    get themes_url
+    assert_response :success
+  end
 end
